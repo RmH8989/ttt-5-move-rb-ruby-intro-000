470349085bf4bee831cc80401e7e47796436c93e
Rhett Hupka <rhett.h89@gmail.com>
2021-06-24 03:08:33 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,6 +10,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, user_input, first_player = "X")
-  board[user_input] = first_player = "X"
+def move(board, user_input, first_player = "O")
+  board[user_input] = first_player = "O"
 end
